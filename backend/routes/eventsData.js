8cf3cfe01b1630f7b890d1a7e0d6255487bfb05f
Oscar Lopez <70294673+OLopez2020@users.noreply.github.com>
2022-10-06 21:49:38 -0500

    eventData updated

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -6,7 +6,7 @@ let { eventdata } = require("../models/models");
 
 //GET all entries
 router.get("/", (req, res, next) => { 
-    eventdata.find( 
+    eventdata.find( {organization:process.env.ORGANIZATION},
         (error, data) => {
             if (error) {
                 return next(error);
@@ -19,7 +19,9 @@ router.get("/", (req, res, next) => {
 
 //GET single entry by ID
 router.get("/id/:id", (req, res, next) => { 
-    eventdata.find({ _id: req.params.id }, (error, data) => {
+    eventdata.find({ organization:process.env.ORGANIZATION,
+        _id: req.params.id }, 
+    (error, data) => {
         if (error) {
             return next(error)
         } else {
@@ -33,9 +35,11 @@ router.get("/id/:id", (req, res, next) => {
 router.get("/search/", (req, res, next) => { 
     let dbQuery = "";
     if (req.query["searchBy"] === 'name') {
-        dbQuery = { eventName: { $regex: `^${req.query["eventName"]}`, $options: "i" } }
+        dbQuery = { organization:process.env.ORGANIZATION, 
+            eventName: { $regex: `^${req.query["eventName"]}`, $options: "i" } }
     } else if (req.query["searchBy"] === 'date') {
         dbQuery = {
+            organization:process.env.ORGANIZATION, 
             date:  req.query["eventDate"]
         }
     };
@@ -54,7 +58,8 @@ router.get("/search/", (req, res, next) => {
 //GET events for which a client is signed up
 router.get("/client/:id", (req, res, next) => { 
     eventdata.find( 
-        { attendees: req.params.id }, 
+        { organization:process.env.ORGANIZATION, 
+            attendees: req.params.id }, 
         (error, data) => { 
             if (error) {
                 return next(error);
@@ -67,8 +72,10 @@ router.get("/client/:id", (req, res, next) => {
 
 //POST
 router.post("/", (req, res, next) => { 
+    var data = req.body
+    data['organization'] = process.env.ORGANIZATION
     eventdata.create( 
-        req.body, 
+        data, 
         (error, data) => { 
             if (error) {
                 return next(error);
@@ -81,9 +88,11 @@ router.post("/", (req, res, next) => {
 
 //PUT
 router.put("/:id", (req, res, next) => {
+    var data = req.body
+    data['organization'] = process.env.ORGANIZATION
     eventdata.findOneAndUpdate(
         { _id: req.params.id },
-        req.body,
+        data,
         (error, data) => {
             if (error) {
                 return next(error);
@@ -94,22 +103,25 @@ router.put("/:id", (req, res, next) => {
     );
 });
 
+
 //PUT add attendee to event
 router.put("/addAttendee/:id", (req, res, next) => {
     //only add attendee if not yet signed uo
     eventdata.find( 
-        { _id: req.params.id, attendees: req.body.attendee }, 
+        { organization:process.env.ORGANIZATION, 
+            _id: req.params.id, 
+            attendees: req.body.attendee }, 
         (error, data) => { 
             if (error) {
                 return next(error);
             } else {
                 if (data.length == 0) {
                     eventdata.updateOne(
-                        { _id: req.params.id }, 
+                        { _id: req.params.id ,
+                            organization:process.env.ORGANIZATION, }, 
                         { $push: { attendees: req.body.attendee } },
                         (error, data) => {
                             if (error) {
-                                consol
                                 return next(error);
                             } else {
                                 res.json(data);
@@ -121,7 +133,9 @@ router.put("/addAttendee/:id", (req, res, next) => {
             }
         }
     );
+    
 });
+
 
 //DELETE event data document based on id param Jacob Hui
 router.delete("/:id", (req, res, next) => { 
@@ -152,5 +166,27 @@ router.delete("/delAttendee/:id", (req, res, next) => {
         }
     );
 });
+
+//GET all the events & attendee Ex...Body eventName: "event"
+//Also how many clients signed up for each event for the last 2 months
+//OscarLopez
+router.get("/report", (req, res, next) => { 
+    var d = new Date();
+    d.setMonth(d.getMonth() - 2); //2 month ago
+    eventdata.aggregate([
+        {$match:{organization:process.env.ORGANIZATION,
+            date:{$gte:d}}},
+        {$project:{_id:1, attendee:{$size:"$attendees"}}}
+    ] ,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
 
 module.exports = router;
