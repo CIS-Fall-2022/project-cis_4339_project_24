42823f8f57d8c027830f89458ca101c2395ae2a6
Oscar Lopez <70294673+OLopez2020@users.noreply.github.com>
2022-11-12 18:53:04 -0600

    BarChart
    Oscar Lopez

---
+++ frontend/src/components/BarChartComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <canvas ref="myChart"></canvas>
+  </template>
+  
+  <script>
+  import { Chart, registerables } from 'chart.js'
+  
+  Chart.register(...registerables);
+  export default {
+    props: {
+      label: {
+        type: Array,
+      },
+      chartData: {
+        type: Array,
+      },
+    },
+    async mounted() {
+      console.log(this.chartData);
+      console.log("theses are the labels" + this.label)
+      new Chart(this.$refs.myChart, {
+        type: "bar",
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: "Attendees",
+              backgroundColor: "rgba(144,238,144 , 0.9 )",
+              data: this.chartData,
+            },
+          ],
+        },
+        options: {
+          scales: {
+              y: {
+                  max: 10
+              }
+          }
+        }
+      });
+    },
+  };
+  </script>
